006650043e16bb2dd80bf1fa5dc7e0a00de5860f
Shinya Ishikawa <ishikawa@rt-net.jp>
2022-11-30 04:14:32 +0900

    Add ResourceStreamer

--- resource-stream/main.js
+++ resource-stream/main.js
@@ -1,61 +1,70 @@
 /*
-	Streaming from local audio samples
-	
-		queue audio samples in chunks
-		keep about one second of audio queued
-		audio queued as raw samples (skips MAUD header)
-		calcuates RMS of each block after dequeuing
-
-		streaming from a resource is not needed for playback: the full resource could be queued.
-		streaming is done here to demonstrate streaming audio independent of network.
-		streaming is convenient for calculating power in increments
-		reading from a resource could be replaced with reading from a file  
+ * Copyright (c) 2022  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ * 
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
  */
 
 import AudioOut from "pins/audioout"
-import Resource from "Resource";
-
-const samples = new Resource("googletts_16bit_mono_11025hz.maud");
-samples.position = 12;		// skip maud header
-
-const sampleRate = 11025;
-const bytesPerSample = 2;
-const targetBytesQueued = sampleRate * bytesPerSample;
-const bytesPerBlock = Math.idiv(targetBytesQueued, 8);
-if (bytesPerBlock % bytesPerSample)
-	throw new Error("invalid bytesPerBlock")
-let bytesQueued = 0; 
-
-const audio = new AudioOut({});
-audio.playing = [];
-audio.callback = function(bytes) {
-	if (!bytes) {
-		trace('...done\n');
-		return;
-	}
-	bytesQueued -= bytes;
-	const played = this.playing.shift();
-	const power = calculatePower(played);
-	trace("power " + power + "\n");
-	fillQueue();
-};
-fillQueue();
-audio.start();
-
-function fillQueue() {
-	while ((bytesQueued < targetBytesQueued) &&
-			(samples.position < samples.byteLength) &&
-			(audio.length(0) >= 2)) {
-		const use = Math.min(targetBytesQueued - bytesQueued, bytesPerBlock);
-		const slice = samples.slice(samples.position, samples.position + use, false);
-		samples.position += slice.byteLength;
-		audio.enqueue(0, AudioOut.RawSamples, slice, 1, 0, slice.byteLength / bytesPerSample);
-		audio.enqueue(0, AudioOut.Callback, slice.byteLength);
-		bytesQueued += slice.byteLength;
-		audio.playing.push(slice);
-		if (samples.position === samples.byteLength)
-			audio.enqueue(0, AudioOut.Callback, 0);
-	}
-}
+import ResourceStreamer from "resourcestreamer";
 
 function calculatePower(samplea) @ "xs_calculatePower";
+
+const audio = new AudioOut({});
+let streamer
+
+async function stream() {
+	return new Promise((resolve, reject) => {
+		if (streamer != null) {
+			reject(new Error("already playing"));
+			return
+		}
+		streamer = new ResourceStreamer({
+		path: "googletts_16bit_mono_11025hz.maud",
+		audio: {
+			out: audio,
+			stream: 0,
+			sampleRate: 11025
+		},
+		onPlayed(buffer) {
+			const power = calculatePower(buffer);
+			trace("power " + Math.round(power) + "\n");
+		},
+		onReady(state) {
+			trace(`Ready: ${state}\n`);
+			if (state)
+				audio.start();
+			else
+				audio.stop();
+		},
+		onError(e) {
+			trace("ERROR: ", e, "\n");
+			streamer = null
+			reject(new Error("unknown error occured"))
+		},
+		onDone() {
+			trace("DONE\n");
+			streamer.close()
+			streamer = null
+			resolve()
+		}
+	});
+})
+}
+
+(async function main() {
+	let count = 0
+	while (true){
+		trace(`play: ${count++}\n`)
+		let willPlay = stream();
+		let willFail = stream().catch(e => {trace(`error: ${e.message}\n`)}) // error: already playing
+		await Promise.allSettled([willPlay, willFail])
+	}
+})()
